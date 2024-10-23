36c6f0e83dffffc82f9ca17a674f24d4e18b3841
karlafehir <fehirkarla@gmail.com>
2024-10-23 14:13:13 +0200

    api fix

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -13,19 +13,19 @@ export class EmployeeService {
     constructor(private http: HttpClient) { }
 
     getAllEmployees(): Observable<Employee[]> {
-      return this.http.get<Employee[]>(`${this.apiUrl}/api/Employees/GetAllEmployees`);
+      return this.http.get<Employee[]>(`${this.apiUrl}/api/Employee/GetAllEmployees`);
     }
 
     getEmployeeById(id: number): Observable<Employee>{
-      return this.http.get<Employee>(`${this.apiUrl}/api/Employees/GetEmployeeById/${id}`);
+      return this.http.get<Employee>(`${this.apiUrl}/api/Employee/GetEmployeeById/${id}`);
     }
 
     addEmployee(employee: Employee): Observable<Employee> {
-      return this.http.post<Employee>(`${this.apiUrl}/api/Employees/AddEmployee`, employee);
+      return this.http.post<Employee>(`${this.apiUrl}/api/Employee/AddEmployee`, employee);
     }
 
     updateEmployee(employee: Employee): Observable<Employee> {
-      return this.http.put<Employee>(`${this.apiUrl}/api/Employees/UpdateEmployee/${employee.id}`, employee);
+      return this.http.put<Employee>(`${this.apiUrl}/api/Employee/UpdateEmployee/${employee.id}`, employee);
     }
     
 }
