05e9b7a9d0c2b11744d9dfb826bf9ec38561fd6e
karlafehir <fehirkarla@gmail.com>
2024-10-24 15:45:40 +0200

    employee profile - refresh info after update

--- src/app/components/employees/employee-profile/employee-profile.component.ts
+++ src/app/components/employees/employee-profile/employee-profile.component.ts
@@ -45,6 +45,7 @@ export class EmployeeProfileComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
         console.log('Employee updated successfully');
+        this.getEmployeeById(employee.id);
       }
     });
   }
