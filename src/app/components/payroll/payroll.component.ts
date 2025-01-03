fe6ca71d3f8e85034113e53eac158fe0fdf8c688
karlafehir <fehirkarla@gmail.com>
2025-01-03 11:43:14 +0100

    payroll - added datatable and show data

--- src/app/components/payroll/payroll.component.ts
+++ src/app/components/payroll/payroll.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../../models/employeeModel';
 import { Router } from '@angular/router';
 import { fadeInAnimation } from '../../shared/animations/fadeInAnimation';
-import { EmployeeService } from '../../services/employee.service';
-import { PayrollEmployee } from '../../models/payrollEmployeeModel'; // Ensure you import the model
+import { PayrollService } from '../../services/payroll.service';
+import { Payroll } from '../../models/payrollModel';
 
 @Component({
   selector: 'app-payroll',
@@ -12,39 +11,29 @@ import { PayrollEmployee } from '../../models/payrollEmployeeModel'; // Ensure y
   animations: [fadeInAnimation]
 })
 export class PayrollComponent implements OnInit {
-  payrollEmployees: PayrollEmployee[] = [];
+  payrolls: Payroll[] = [];
   searchText: string = '';
-  displayedColumns: string[] = ['name', 'department', 'salary', 'paymentDate', 'paymentMethod'];
+  displayedColumns: string[] = ['name', 'salary', 'bonus', 'isComplete'];
 
-  constructor(private router: Router, private employeeService: EmployeeService) {}
+  constructor(private router: Router, private payrollService: PayrollService) {}
 
   ngOnInit(): void {
-    this.getAllPayrollEmployees();
+    this.getAllPayrolls();
   }
 
-  getAllPayrollEmployees(): void {
-    this.employeeService.getAllEmployees().subscribe(response => {
-      this.payrollEmployees = response.map(employee => ({
-        employee,
-        status: 'pending',
-        salary: 0,
-        paymentDate: null, 
-        paymentMethod: 'Bank Transfer' 
-      }));
+  getAllPayrolls(): void {
+    this.payrollService.getAllPayrolls().subscribe((response: Payroll[]) => {
+      this.payrolls = response;
+      console.log(this.payrolls);
     });
-    console.log(this.payrollEmployees);
   }
 
-  getFilteredPayrollEmployees(): PayrollEmployee[] {
+  getFilteredPayrolls(): Payroll[] {
     if (!this.searchText) {
-      return this.payrollEmployees;
+      return this.payrolls;
     }
-    return this.payrollEmployees.filter(payrollEmployee =>
-      (payrollEmployee.employee.firstName + ' ' + payrollEmployee.employee.lastName).toLowerCase().includes(this.searchText.toLowerCase())
+    return this.payrolls.filter(payroll =>
+      (payroll.employee.firstName + ' ' + payroll.employee.lastName).toLowerCase().includes(this.searchText.toLowerCase())
     );
   }
-
-  // goToEmployeeProfile(employee: Employee): void {
-  //   this.router.navigate(['employee-profile', employee.id]);
-  // }
 }
