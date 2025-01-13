4a5019399c94ec311e70abd3ccf5377a916428b2
karlafehir <fehirkarla@gmail.com>
2025-01-13 14:13:58 +0100

    employees datatable - remove id, show department name, job title

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -3,8 +3,10 @@ import { Employee } from '../../models/employeeModel';
 import { Router } from '@angular/router';
 import { fadeInAnimation } from '../../shared/animations/fadeInAnimation';
 import { EmployeeService } from '../../services/employee.service';
+import { DepartmentService } from '../../services/department.service';
 import { MatDialog } from '@angular/material/dialog';
 import { EmployeeFormDialogComponent } from './employee-form-dialog/employee-form-dialog.component';
+import { Department } from '../../models/employeeModel';
 
 @Component({
   selector: 'app-employees',
@@ -13,29 +15,52 @@ import { EmployeeFormDialogComponent } from './employee-form-dialog/employee-for
   animations: [fadeInAnimation]
 })
 export class EmployeesComponent implements OnInit {
-
   employees: Employee[] = [];
+  departments: Department[] = [];
   searchText: string = '';
-  displayedColumns: string[] = ['id', 'name', 'phoneNumber', 'department', 'dateOfHire']; // Adjusted columns
+  displayedColumns: string[] = [ 'name', 'phoneNumber', 'department', 'job']; // Adjusted columns
 
-  constructor(private router: Router, private employeeService: EmployeeService, public dialog: MatDialog) {}
+  constructor(
+    private router: Router,
+    private employeeService: EmployeeService,
+    private departmentService: DepartmentService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.getAllDepartments();
     this.getAllEmployees();
   }
 
   getAllEmployees(): void {
-    this.employeeService.getAllEmployees().subscribe(response => {
+    this.employeeService.getAllEmployees().subscribe((response) => {
       this.employees = response;
     });
+  }
+
+  getAllDepartments(): void {
+    this.departmentService.getAllDepartments().subscribe((response) => {
+      this.departments = response;
+    });
+  }
+
+  getDepartmentName(departmentId: number): string {
+    const department = this.departments.find((d) => d.id === departmentId);
+    return department ? department.name : 'Unknown';
   }
 
   getFilteredEmployees(): Employee[] {
     if (!this.searchText) {
       return this.employees;
     }
-    return this.employees.filter(employee =>
-      (employee.firstName + ' ' + employee.lastName).toLowerCase().includes(this.searchText.toLowerCase())
+    return this.employees.filter((employee) =>
+      (employee.firstName + ' ' + employee.lastName)
+        .toLowerCase()
+        .includes(this.searchText.toLowerCase())
     );
   }
 
@@ -46,10 +71,10 @@ export class EmployeesComponent implements OnInit {
   openDialog(): void {
     const dialogRef = this.dialog.open(EmployeeFormDialogComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         console.log('Dialog closed with result:', result);
-        this.getAllEmployees();
+        this.loadData();
       }
     });
   }
